ed722c84136a23cf3db96b45097d57a664e1bde9
vn0iqb6 <Utpal.Das@walmartlabs.com>
2018-10-22 02:38:20 -0700

    Authentication Added

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports={
-    mongoURI:'mongodb://utpal:qwerty12345@ds137003.mlab.com:37003/mern'
+    mongoURI:'mongodb://utpal:qwerty12345@ds137003.mlab.com:37003/mern',
+    secretOrKey:'secret'
 };
--- server.js
+++ server.js
@@ -3,7 +3,14 @@ const mongoose= require("mongoose");
 const users= require("./routes/api/users")
 const posts = require("./routes/api/posts")
 const profile = require("./routes/api/profile")
+const bodyParser = require("body-parser");
+const passport=require("passport");
+
 const app=express();
+//Body-Parser middleware
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
 //DB config
 const db=require("./config/keys").mongoURI;
 //Connect Mongo DB
