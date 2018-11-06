d6c9d6237f425b9136053901da3a4ae248ba224b
vn0iqb6 <Utpal.Das@walmartlabs.com>
2018-11-06 03:43:05 -0800

    Login, Register, User Schema, profile Schema Added.

--- server.js
+++ server.js
@@ -3,7 +3,14 @@ const mongoose= require("mongoose");
 const users= require("./routes/api/users")
 const posts = require("./routes/api/posts")
 const profile = require("./routes/api/profile")
+const bodyParser = require("body-parser");
+const passport=require("passport");
+
 const app=express();
+//Body-Parser middleware
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
 //DB config
 const db=require("./config/keys").mongoURI;
 //Connect Mongo DB
@@ -11,8 +18,10 @@ mongoose
     .connect(db)
     .then(() => console.log("MongoDb Connected"))
     .catch(err => console.log(err));
-
-app.get('/', (req,res) => res.send("Hello World!!"));
+//Passport Middleware
+app.use(passport.initialize());
+//passport config
+require('./config/passport')(passport);
 //Use routes
 app.use('/api/users', users);
 app.use('/api/profile', profile);
