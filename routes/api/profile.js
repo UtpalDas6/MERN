2f4f996223b856852954b6d8fdcd5cc4dae9a15a
vn0iqb6 <Utpal.Das@walmartlabs.com>
2019-01-02 22:43:37 -0800

    more api routes added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -28,6 +28,62 @@ router.get('/', passport.authenticate('jwt', {session: false }), (req,res) => {
                 errors.noprofile="User Profile Not Found";
                 return res.status(404).json(errors);
             }
+            res.json(profile);
+        })
+        .catch(err => res.status(404).json(err))
+});
+
+// @route GET api/profile/user/:user_id
+// @desc GET profile by user_id
+// @access PUBLIC
+router.get('/user/:user_id', (req, res) => {
+    const errors = {};
+
+    Profile.findOne({ user: req.params.user_id })
+        .populate('user', ['name', 'avatar'])
+        .then(profile => {
+            if (!profile) {
+                errors.noprofile = "There is no profile found for this user";
+                return res.status(404).json(errors);
+            }
+            res.json(profile);
+        })
+        .catch(err => res.status(404).json({profile:'There is no profile for this user'}))
+});
+
+// @route GET api/profile/all
+// @desc GET all profiles
+// @access PUBLIC
+router.get('/all', (req, res) => {
+    const errors = {};
+
+    Profile.find()
+        .populate('user', ['name', 'avatar'])
+        .then(profile => {
+            if (!profile) {
+                errors.noprofile ='There are no profiles';
+                return res.status(404).json(errors);
+            }
+            res.json(profile);
+        })
+        .catch(err => res.status(404).json({ profile: 'There are no profiles' }))
+});
+
+
+// @route GET api/profile/handle:handle
+// @desc Create user profile
+// @access PUBLIC
+router.get('/handle/:handle', (req, res) => {
+    const errors = {};
+
+    Profile.findOne({ handle: req.params.handle })
+        .populate('user', ['name', 'avatar'])
+        .then(profile => {
+            if (!profile) {
+                errors.noprofile = "There is no profile found for this user";
+                return res.status(404).json(errors);
+            }
+            res.json(profile);
         })
         .catch(err => res.status(404).json(err))
 });
